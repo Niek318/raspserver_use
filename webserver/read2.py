2417884ec24b80d7b57e36e0b48dbfbf3270ec38
Niek318 <38814969+Niek318@users.noreply.github.com>
2020-02-18 23:25:53 +0100

    xx

--- webserver/read2.py
+++ webserver/read2.py
@@ -20,35 +20,57 @@ print(sensorids)
 
 while True:
 
-    for sensor in sensorids:
-        # tfile = open("/sys/bus/w1/devices/"+ sensorids[sensor] +"/w1_slave") #RPi 1,2 met oude kernel.
-        tfile = open(
-            "/sys/bus/w1/devices/" + sensor + "/w1_slave"
-        )  # RPi 2,3 met nieuwe kernel.
-        # Lees alle dat uit het "bestand" in een variabele.
-        text = tfile.read()
-        # Sluit het "bestand" nadat we het gelezen hebben.
-        tfile.close()
-        # We gaan nu de tekst splitsen per nieuwe regel (\n)
-        # en we selecteren de 2e regel [1] (1e regel = [0])
-        secondline = text.split("\n")[1]
-        # Splits de regel in "woorden", er wordt gespleten op de spaties.
-        # We selecteren hier het 10 "woord" [9] (tellend vanaf 0)
-        temperaturedata = secondline.split(" ")[9]
-        # De eerste 2 karakters zijn "t=", deze moeten we weghalen.
-        # we maken meteen van de string een integer (nummer).
-        temperature = float(temperaturedata[2:])
-        # De temperatuurwaarde moeten we delen door 1000 voor de juiste waarde.
-        temp[sensor] = temperature / 1000
-        # print de gegevens naar de console.
-        print "sensor", sensor, "=", temp[sensor], "graden."
+    tfile = open("/sys/bus/w1/devices/" + sensorids[0] + "/w1_slave")
 
+    tfile2 = open("/sys/bus/w1/devices/" + sensorids[1] + "/w1_slave")
 
-        with sql.connect("raspsensors.db") as con:
-            cur = con.cursor()
-            cur.execute(
-                "INSERT INTO sensordata (cold_water, hot_water, flow, currentdate, currenttime, name) VALUES (?,?,?,date('now'),time('now'),?)",
-                    (temp[sensor], temp[sensor], 1, "testname"),
-                )
+    text = tfile.read()
+    tfile.close()
 
-            con.commit()
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    temperature = temperature / 1000
+
+    text2 = tfile2.read()
+    tfile2.close()
+
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature2 = float(temperaturedata[2:])
+    temperature2 = temperature / 1000
+
+    print(temperature, temperature2)
+
+    # for sensor in sensorids:
+    #     # tfile = open("/sys/bus/w1/devices/"+ sensorids[sensor] +"/w1_slave") #RPi 1,2 met oude kernel.
+    #     tfile = open(
+    #         "/sys/bus/w1/devices/" + sensor + "/w1_slave"
+    #     )  # RPi 2,3 met nieuwe kernel.
+    #     # Lees alle dat uit het "bestand" in een variabele.
+    #     text = tfile.read()
+    #     # Sluit het "bestand" nadat we het gelezen hebben.
+    #     tfile.close()
+    #     # We gaan nu de tekst splitsen per nieuwe regel (\n)
+    #     # en we selecteren de 2e regel [1] (1e regel = [0])
+    #     secondline = text.split("\n")[1]
+    #     # Splits de regel in "woorden", er wordt gespleten op de spaties.
+    #     # We selecteren hier het 10 "woord" [9] (tellend vanaf 0)
+    #     temperaturedata = secondline.split(" ")[9]
+    #     # De eerste 2 karakters zijn "t=", deze moeten we weghalen.
+    #     # we maken meteen van de string een integer (nummer).
+    #     temperature = float(temperaturedata[2:])
+    #     # De temperatuurwaarde moeten we delen door 1000 voor de juiste waarde.
+    #     temp[sensor] = temperature / 1000
+
+    #     # print de gegevens naar de console.
+    #     print "sensor", sensor, "=", temp[sensor], "graden."
+
+    #     with sql.connect("raspsensors.db") as con:
+    #         cur = con.cursor()
+    #         cur.execute(
+    #             "INSERT INTO sensordata (cold_water, hot_water, flow, currentdate, currenttime, name) VALUES (?,?,?,date('now'),time('now'),?)",
+    #                 (temp[sensor], temp[sensor], 1, "testname"),
+    #             )
+
+    #         con.commit()
