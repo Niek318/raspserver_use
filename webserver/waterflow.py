c61a34c287f754c51733bc5e38d88ca15a49cb4c
Niek318 <38814969+Niek318@users.noreply.github.com>
2020-03-02 15:16:28 +0100

    Update waterflow.py

--- webserver/waterflow.py
+++ webserver/waterflow.py
@@ -35,3 +35,34 @@ GPIO.cleanup()
 print("Done")
 
 # pip install RPi.GPIO
+
+# import time
+# import pigpio
+
+# FLOW_GPIO = 26
+# RUN_TIME = 60.0
+# SAMPLE_TIME = 1.0
+
+# pi = pigpio.pi() # connect to Pi
+# if not pi.connected:
+#    exit()
+
+# pi.set_mode(FLOW_GPIO, pigpio.INPUT)
+# pi.set_pull_up_down(FLOW_GPIO, pigpio.PUD_UP)
+
+# callback = pi.callback(FLOW_GPIO) # default tally callback
+
+# stop = time.time() + RUN_TIME
+
+# try:
+
+#    while time.time() < stop:
+
+#       time.sleep(SAMPLE_TIME)
+
+#       print("tally={}".format(callback.tally()))
+
+# except KeyboardInterrupt:
+#    pass
+
+# print("\nexiting")
