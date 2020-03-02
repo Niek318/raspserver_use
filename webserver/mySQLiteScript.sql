2ebc85a26c31954c864f18f082eb9ed560526871
Niek318 <38814969+Niek318@users.noreply.github.com>
2020-03-02 23:15:26 +0100

    Update mySQLiteScript.sql

--- webserver/mySQLiteScript.sql
+++ webserver/mySQLiteScript.sql
@@ -1,9 +1,8 @@
-CREATE TABLE sensordata (
- id INTEGER PRIMARY KEY,
- cold_water NUMERIC,
- hot_water NUMERIC,
- flow NUMERIC,
- currentdate DATE,
- currenttime TIME,
- name TEXT
+CREATE TABLE sensordata
+(
+    id INTEGER PRIMARY KEY,
+    cold_water NUMERIC,
+    hot_water NUMERIC,
+    flow NUMERIC,
+    shower_time NUMERIC
 );
